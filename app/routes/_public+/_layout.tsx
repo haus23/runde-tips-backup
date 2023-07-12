2964b1aeae1b61c6ef310340dbea1cdc8ec1a76b
Micha Buchholz <micha@haus23.net>
2023-07-12 22:35:45 +0200

    feat: Create minimal styled haeder. Resolves #9

--- app/routes/_public+/_layout.tsx
+++ app/routes/_public+/_layout.tsx
@@ -1,19 +1,10 @@
-import { Link, Outlet } from '@remix-run/react';
-import { useOptionalUser } from '~/utils/user';
+import { Outlet } from '@remix-run/react';
+import { Header } from './header';
 
 export default function PublicLayout() {
-  const user = useOptionalUser();
-
   return (
     <>
-      <header>
-        <h1 className="text-2xl font-semibold">runde.tips</h1>
-        {user ? (
-          <Link to={'/logout'}>Logout</Link>
-        ) : (
-          <Link to={'/login'}>Login</Link>
-        )}
-      </header>
+      <Header />
       <main>
         <Outlet />
       </main>
---
+++ app/routes/_public+/header.tsx
@@ -0,0 +1,54 @@
+import { Link as RouterLink, NavLink as RouterNavLink } from '@remix-run/react';
+import { Link } from 'react-aria-components';
+import LogoImage from '~/assets/logo.svg';
+import { useOptionalUser } from '~/utils/user';
+
+const navItems = [
+  { label: 'Tabelle', viewSegment: 'tabelle' },
+  { label: 'Spieler', viewSegment: 'tipps/spieler' },
+  { label: 'Spiele', viewSegment: 'tipps/spiele' },
+];
+
+export function Header() {
+  const user = useOptionalUser();
+  return (
+    <header className="h-16 px-1 border-b">
+      {/* Desktop Nav */}
+      <div className="flex h-full items-center gap-x-1">
+        <nav className="flex grow items-center justify-between">
+          <Link className="flex">
+            <RouterLink to="/">
+              <div className="flex shrink-0 items-center pr-1">
+                <svg className="h-12 w-12 fill-current">
+                  <use href={`${LogoImage}#logo`} />
+                </svg>
+                <span>runde.tips</span>
+              </div>
+            </RouterLink>
+          </Link>
+          <div className="flex gap-x-1">
+            {navItems.map((item, ix) => (
+              <Link key={ix} className="px-1">
+                <RouterNavLink to={item.viewSegment}>
+                  {item.label}
+                </RouterNavLink>
+              </Link>
+            ))}
+            <Link className="px-1">
+              {user ? (
+                <RouterLink to="/manager">Manager</RouterLink>
+              ) : (
+                <RouterNavLink to="/login">Log in</RouterNavLink>
+              )}
+            </Link>
+          </div>
+        </nav>
+        <div>
+          {/* Action Buttons: User-Menu(?) Theme-Wechsel, Chat-Icon(?), Turnier-Suche(?) */}
+        </div>
+      </div>
+      {/* Mobile Nav */}
+      <div className="flex sm:hidden"></div>
+    </header>
+  );
+}
